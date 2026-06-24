f2175d1209bd1d484195c36b5d8797bf1a032658
Victor Azevedo <hello@exemple.com>
2026-06-24 09:32:51 +0200

    Fix TestWith use, add testdox to config

--- tests/Controller/Front/HomeControllerTestCase.php
+++ tests/Controller/Front/HomeControllerTestCase.php
@@ -3,7 +3,7 @@
 namespace App\Tests\Controller\Front;
 
 use App\Tests\AbstractWebTestCase;
-use PHPUnit\Metadata\TestWith;
+use PHPUnit\Framework\Attributes\TestWith;
 
 class HomeControllerTestCase extends AbstractWebTestCase
 {
@@ -20,30 +20,30 @@ class HomeControllerTestCase extends AbstractWebTestCase
         $this->assertSelectorTextContains('h1', 'SteamIsh V3');
     }
 
-    #[\PHPUnit\Framework\Attributes\TestWith([0, ''])]
-    #[\PHPUnit\Framework\Attributes\TestWith([1, '0'])]
-    #[\PHPUnit\Framework\Attributes\TestWith([2, 'Se connecter'])]
+    #[TestWith([0, ''])]
+    #[TestWith([1, '0'])]
+    #[TestWith([2, 'Se connecter'])]
     public function testHeaderLinks(int $order, string $text): void
     {
         $link = $this->crawler->filter('a')->eq($order)->text();
         $this->assertEquals($text, $link);
     }
 
-    #[\PHPUnit\Framework\Attributes\TestWith([0, 'Recherchez un jeu, éditeur...'])]
-    #[\PHPUnit\Framework\Attributes\TestWith([1, 'Les dernières sorties'])]
-    #[\PHPUnit\Framework\Attributes\TestWith([2, 'Les meilleures offres'])]
-    #[\PHPUnit\Framework\Attributes\TestWith([3, 'Les tendances actuelles'])]
-    #[\PHPUnit\Framework\Attributes\TestWith([4, 'Les plus joués'])]
+    #[TestWith([0, 'Recherchez un jeu, éditeur...'])]
+    #[TestWith([1, 'Les dernières sorties'])]
+    #[TestWith([2, 'Les meilleures offres'])]
+    #[TestWith([3, 'Les tendances actuelles'])]
+    #[TestWith([4, 'Les plus joués'])]
     public function testCategoryTitles(int $order, string $text): void
     {
         $title = $this->crawler->filter('h2')->eq($order)->text();
         $this->assertEquals($text, $title);
     }
 
-    #[\PHPUnit\Framework\Attributes\TestWith(['Les dernières sorties'])]
-    #[\PHPUnit\Framework\Attributes\TestWith(['Les meilleures offres'])]
-    #[\PHPUnit\Framework\Attributes\TestWith(['Les tendances actuelles'])]
-    #[\PHPUnit\Framework\Attributes\TestWith(['Les plus joués'])]
+    #[TestWith(['Les dernières sorties'])]
+    #[TestWith(['Les meilleures offres'])]
+    #[TestWith(['Les tendances actuelles'])]
+    #[TestWith(['Les plus joués'])]
     public function testGamesAmountInCategory(string $id): void
     {
         $games = $this->crawler->filter('[data-test-id="' . $id . '"] a');
