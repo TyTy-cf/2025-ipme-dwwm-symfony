34c8bab3af64897a887c030da100f9f3c3b31524
Kevin Tourret <kevin@drosalys.fr>
2026-06-24 11:32:05 +0200

    correction exo

--- tests/Correction/AbstractKernelTestCase.php
+++ tests/Correction/AbstractKernelTestCase.php
@@ -2,14 +2,26 @@
 
 namespace App\Tests\Correction;
 
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Session\Storage\MockArraySessionStorage;
 
-class AbstractKernelTestCase extends WebTestCase
+class AbstractKernelTestCase extends KernelTestCase
 {
 
     protected function setUp(): void
     {
+        parent::setUp();
         self::bootKernel();
+
+        $session = new Session(new MockArraySessionStorage());
+        $request = new Request();
+        $request->setSession($session);
+
+        $requestStack = static::getContainer()->get(RequestStack::class);
+        $requestStack->push($request);
     }
 
     protected function get(string $className): null|object
