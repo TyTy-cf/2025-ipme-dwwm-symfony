f669ecabb5b74686f2c34aae41c828f134211ae6
Your Name <you@example.com>
2026-06-24 11:58:49 +0200

    exo tests

--- tests/AbstractKernelTestCaseTest.php
+++ tests/AbstractKernelTestCaseTest.php
@@ -6,6 +6,10 @@ namespace App\Tests;
 
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Session\Storage\MockArraySessionStorage;
 
 class AbstractKernelTestCaseTest extends KernelTestCase
 {
@@ -22,5 +26,20 @@ class AbstractKernelTestCaseTest extends KernelTestCase
 
         $this->assertNotEmpty($results);
         $this->assertCount($limit, $results);
+    }
+
+    protected function get(string $className): null|object
+    {
+        return static::getContainer()->get($className);
+    }
+
+    protected function setUpRequestStack(): void
+    {
+        $session = new Session(new MockArraySessionStorage());
+        $request = new Request();
+        $request->setSession($session);
+
+        $requestStack = static::getContainer()->get(RequestStack::class);
+        $requestStack->push($request);
     }
 }
---
+++ tests/Service/SessionCartServiceTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Tests\Service;
+
+use App\Repository\GameRepository;
+use App\Service\SessionCartService;
+use App\Tests\AbstractKernelTestCaseTest;
+use Symfony\Component\Serializer\Exception\ExceptionInterface;
+
+class SessionCartServiceTest extends AbstractKernelTestCaseTest
+{
+    private SessionCartService $sessionCartService;
+    private GameRepository $gameRepository;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->setUpRequestStack();
+        $this->sessionCartService = $this->get(SessionCartService::class);
+        $this->gameRepository = $this->get(GameRepository::class);
+    }
+
+    /**
+     * @throws ExceptionInterface
+     */
+    public function testAddItemToCartSession(): void
+    {
+        $this->addGameToCart(2);
+        $this->addGameToCart(3);
+
+        $this->assertEquals(2, $this->sessionCartService->getCartQty());
+    }
+
+    /**
+     * @throws ExceptionInterface
+     */
+    private function addGameToCart(int $id): void {
+        $game = $this->gameRepository->find($id);
+        $this->sessionCartService->addItemToCart($game);
+    }
+
+    /**
+     * @throws ExceptionInterface
+     */
+    public function testCartQuantity(): void
+    {
+        $this->addGameToCart(5);
+        $quantity = $this->sessionCartService->getCartQty();
+
+        $this->assertEquals(1, $quantity);
+    }
+}
