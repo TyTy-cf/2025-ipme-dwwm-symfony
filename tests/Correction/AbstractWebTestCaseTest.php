b24f79ccb099fa34d2817cbcd82bb33a7efc6588
Kevin Tourret <kevin@drosalys.fr>
2026-06-24 15:10:52 +0200

    add correction exo 5

--- tests/Correction/AbstractWebTestCaseTest.php
+++ tests/Correction/AbstractWebTestCaseTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Tests\Correction;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\BrowserKit\AbstractBrowser;
@@ -12,15 +13,28 @@ use Symfony\Component\DomCrawler\Crawler;
 class AbstractWebTestCaseTest extends WebTestCase
 {
     static string $HOME = '/';
+    static string $PROFILE = '/profil/morgan93';
 
     protected string $defaultUrl;
     protected KernelBrowser|AbstractBrowser|null $client;
     protected Crawler $crawler;
+    protected ?UserRepository $userRepository = null;
 
     protected function setUp(): void
     {
         $this->client = static::createClient();
         $this->crawler = $this->client->request('GET', $this->defaultUrl);
+    }
+
+    protected function login(?string $email = null): void
+    {
+        if ($this->userRepository === null) {
+            $this->userRepository = static::getContainer()->get(UserRepository::class);
+        }
+
+        if (null !== $user = $this->userRepository->findOneBy(['email' => $email])) {
+            $this->client->loginUser($user);
+        }
     }
 
     public function testAccessOk(): void
