b2d770b42af20aa3b4cf57546299846d5d6013e8
Radia <drineradiaa@gmail.com>
2026-06-25 14:25:19 +0200

    test get

--- tests/Correction/Controller/Api/Country/GetControllerTest.php
+++ tests/Correction/Controller/Api/Country/GetControllerTest.php
@@ -2,48 +2,31 @@
 
 namespace App\Tests\Correction\Controller\Api\Country;
 
-use App\Entity\Country;
 use App\Tests\Correction\AbstractApiTestCaseTest;
-use Doctrine\ORM\EntityManagerInterface;
 
 class GetControllerTest extends AbstractApiTestCaseTest
 {
-
-    protected Country $country;
-
     protected function setUp(): void
     {
         parent::setUp();
-
-        $em = static::getContainer()->get(EntityManagerInterface::class);
-
-        $country = new Country();
-        $country->setCode('TE');
-        $country->setName('Test Country');
-
-        $em->persist($country);
-        $em->flush();
-
-        $this->country = $country;
+        $this->defaultUrl = self::$COUNTRY;
     }
 
     public function testGetCountryOk(): void
     {
         $token = $this->getAuthToken('kevin@drosalys.fr', '12345');
 
-        $response = $this->client->request('GET', self::$COUNTRY . '/' . $this->country->getId(), [
+        $this->client->request('GET', self::$COUNTRY . '/' . $this->country->getId(), [
             'headers' => [
                 'Authorization' => 'Bearer ' . $token,
-                'Accept' => 'application/json',
+                'Content-Type' => 'application/ld+json',
             ],
         ]);
 
-
         $this->assertResponseStatusCodeSame(200);
-
-        $data = $response->toArray();
-        $this->assertSame('TE', $data['code']);
-        $this->assertSame('Test Country', $data['name']);
+        $this->assertJsonContains([
+            'code' => 'TE',
+            'name' => 'Test Country',
+        ]);
     }
-
 }
